4b73f65d94dea5a554529792220f5654dbac73f0
Bharat Kulkarni <49195030+bharatk101-dckap@users.noreply.github.com>
2021-04-13 13:50:41 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,15 @@
+class DevelopmentConfig:
+    DEBUG = True
+    DB_NAME = 'vizb'
+    DB_HOST = 'vizb.cxwyrgj7u6y5.us-west-2.rds.amazonaws.com'
+    DB_USER='postgres'
+    DB_PASSWORD='Vizb89kcmdDcerRdk12kw'
+    AUTH_KEY = '|d1kpI#.9s!HA#"}KSc&di7}9=mZ/8u(.*BA@YINxCOU~qc{e:NO`RDOZ+2.@*'
+    STRIPE_PUBLISHABLE_KEY = 'pk_test_ItfCPBC7ks34Xp1ymZGewGJj00XmtpTGXn'
+    STRIPE_SECRET_KEY = 'sk_test_kSjG80CtigVq2RHEm8IZDwa000fNr3XpLG'
+    DOWNLOAD_HOST = 'fapi.vizb.ai'
+    SCHEDULER = False
+    SCHEDULER_TIME = 10  # seconds
+    SERVER_NAME_MDB = "LIVE_8_1"
+    IS_MULTIPLE = True
+    DATA_PRO_HOST='https://datapro.vizb.ai'
